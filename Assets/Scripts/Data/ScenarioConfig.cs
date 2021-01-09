45bef06b3439480e2afdaa66bfae2b3b949d4ee0
Juan Diaz <juan.rod.diaz@gmail.com>
2021-01-09 12:15:54 +0100

    Adding Kenney's Platformer Assets Pixel package. Creates base scenario and platform logic classes.

---
+++ Assets/Scripts/Data/ScenarioConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ScenarioDifficultyData
+{
+    public float Height = 0;
+    public List<BasePlatformLogic> PlatformsAvailable;    
+}
+
+[CreateAssetMenu(menuName = "My Assets/Scenario Config")]
+public class ScenarioConfig : ScriptableObject
+{
+    public List<ScenarioDifficultyData> ScenarioData;
+}
