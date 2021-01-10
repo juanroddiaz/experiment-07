6a4dbac817b635c373fede9a5d54d54778951030
Juan Diaz <juan.rod.diaz@gmail.com>
2021-01-10 12:54:10 +0100

    Basic platform catalogue logic.

---
+++ Assets/Scripts/Data/PlatformCatalogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlatformDifficulty
+{ 
+    Easy = 0,
+    Medium,
+    Hard,
+    DarkSouls,
+}
+
+public class PlatformConfig
+{
+    public string Name;
+    public PlatformDifficulty Difficulty;
+    public GameObject Asset;
+}
+
+[CreateAssetMenu(menuName = "My Assets/Platform Catalogue Config")]
+public class PlatformCatalogue : ScriptableObject
+{
+    public List<PlatformConfig> Platforms;
+}
